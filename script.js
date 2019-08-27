acd840936770534db95e414add5bb039113db2aa
Thomas Theux <t.grikschas@gmail.com>
2019-08-27 16:52:42 +0200

    Updated

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
 var textsArr = [];
+var newText;
 
 var currentIndex = 0;
 
 
-function addText() {
-    var newText = document.getElementById("myText").value;
+function checkTexts() {
+    newText = document.getElementById("myText").value;
 
+    // Check if entered text is empty
     if (newText != "") {
-        document.getElementById("enteredText").innerHTML = newText;
-        textsArr.push(newText);
-
-        console.log(textsArr[currentIndex]);
-        currentIndex++;
+        // Check if user already entered a line for the day
+        if (textsArr[currentIndex] != undefined) {
+            if (confirm('You already entered a line for today. Do you want to overwrite it?')) {
+                replaceLine();
+            }
+        } else {
+            addNewLine();
+        }
     } else {
-        console.log("empty");
+        alert("Please enter a line");
     }
+}
+
+
+function addNewLine() {
+    document.getElementById("enteredText").innerHTML = newText;
+    textsArr.push(newText);
+}
+
+
+function replaceLine() {
+    document.getElementById("enteredText").innerHTML = newText;
+    textsArr[currentIndex] = newText;
 }
